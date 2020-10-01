c1d356174d834ace510d7b8c53a580f249b34602
yxx <youyingxiang@users.noreply.github.com>
2020-10-01 14:20:58 +0000

    Apply fixes from StyleCI

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,18 +1,15 @@
 <?php
 
-if (!function_exists('blog_default_title')) {
-
+if (! function_exists('blog_default_title')) {
     function blog_default_title(): string
     {
-        return "游兴祥博客,技术博客,个人博客,laravel博客系统,php博客系统";
+        return '游兴祥博客,技术博客,个人博客,laravel博客系统,php博客系统';
     }
 }
 
-
-if (!function_exists('blog_default_desc')) {
-
+if (! function_exists('blog_default_desc')) {
     function blog_default_desc(): string
     {
-        return "个人博客模板,博客模板,博客系统,技术博客,个人博客,ERP,laravel博客,php博客,laravel-admin,dcat-admin,laravel-livewire,livewire";
+        return '个人博客模板,博客模板,博客系统,技术博客,个人博客,ERP,laravel博客,php博客,laravel-admin,dcat-admin,laravel-livewire,livewire';
     }
 }
