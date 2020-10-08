01a171e57201be5d6a5e9bfabb386568da18c84f
youxingxiang <1365831278@qq.com>
2020-10-08 15:36:38 +0800

    update

--- resources/views/emails/comment/reply.blade.php
+++ resources/views/emails/comment/reply.blade.php
@@ -1,11 +1,11 @@
 @component('mail::message')
-    {{ $comment->target->content ?? $comment->post->title }}:
-    {{ $comment->content }}
+{{ $comment->target->content ?? $comment->post->title }}:
+{{ $comment->content }}
 
-    @component('mail::button', ['url' => $url])
-        点击查看详情
-    @endcomponent
+@component('mail::button', ['url' => $url])
+    点击查看详情
+@endcomponent
 
-    致敬,<br>
-    {{ config('app.name') }}
+致敬,<br>
+{{ config('app.name') }}
 @endcomponent
