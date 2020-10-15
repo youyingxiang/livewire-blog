4fdbd26d49f3d6a8cd6ebddc0349bcecd16b21c5
youxingxiang <1365831278@qq.com>
2020-10-15 11:01:12 +0800

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,13 +29,13 @@
 
                     <div class="mt-4">
                         <x-jet-label value="{{ __('Password') }}"/>
-                        <x-jet-input class="block mt-1 w-full" type="password" name="password" required
+                        <x-jet-input class="block mt-1 w-full" placeholder="密码长度不得低于8位" type="password" name="password" required
                                      autocomplete="new-password"/>
                     </div>
 
                     <div class="mt-4">
                         <x-jet-label value="{{ __('Confirm Password') }}"/>
-                        <x-jet-input class="block mt-1 w-full" type="password" name="password_confirmation" required
+                        <x-jet-input class="block mt-1 w-full" placeholder="密码长度不得低于8位" type="password" name="password_confirmation" required
                                      autocomplete="new-password"/>
                     </div>
 
