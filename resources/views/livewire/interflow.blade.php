e3423cc9a54c84068fc1e792b8ef44f45b885596
youyingxiang <1365831278@qq.com>
2021-06-15 19:18:30 +0800

    技术交流

--- resources/views/livewire/interflow.blade.php
+++ resources/views/livewire/interflow.blade.php
@@ -3,6 +3,6 @@
         <p class="text-base leading-6 text-gray-600 font-semibold tracking-wide uppercase">技术交流</p>
     </div>
     <div class="py-3 content-center">
-        <img class="m-auto rounded-lg w-48 h-48 flex-shrink-0" src="{{ asset('images/wechat.png') }}">
+        <img class="m-auto rounded-lg w-48 h-48 flex-shrink-0" src="{{ asset('images/qq.png') }}">
     </div>
 </div>
