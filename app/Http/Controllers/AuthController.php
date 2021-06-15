fcd3ee7fbf1a272baf1d1c17d55ef74a31a7fc8b
yxx <youyingxiang@users.noreply.github.com>
2021-06-15 11:18:50 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Services\OauthService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
 use Socialite;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
